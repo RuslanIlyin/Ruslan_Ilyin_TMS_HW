1c079b643d655997914c4ee7daf922808a5070b1
Ruslan Ilyin <ilyin.java@gmail.com>
2022-10-28 12:34:42 +0300

    Task 1 copleted.

---
+++ src/Tasks/Task1.java
@@ -0,0 +1,44 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.println("Введите любое положительное целое число ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println("Вы ввели число " + number);
+
+        int num = 0;
+        String banderOstap = "Вы прямо герой труда";
+
+        if (number > 0 && number < 15) {
+            System.out.println("Группа " + ++num);
+        } else if (number >= 15 && number < 30) {
+            num+=2;
+            System.out.println("Группа " + num);
+        } else if (number >= 30) {
+
+            num+=3;
+            System.out.println("Группа " + num);
+        }
+        else {
+            System.out.println("Вы ввели невереное число ");
+        }
+
+        if (num == 1) {
+            System.out.println(banderOstap);
+        } else if (num == 2) {
+            for (int i = 0; i < num; i++) {
+                System.out.println(banderOstap);
+            }
+                } else if (num ==3) {
+            for (int i = 0; i < num; i++) {
+                System.out.println(banderOstap);
+            }
+        }
+
+
+    }
+}
