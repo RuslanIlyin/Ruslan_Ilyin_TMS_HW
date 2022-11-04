04c7b3f6022ed19f6eff1aafbf3967f265af5b6d
Ruslan Ilyin <ilyin.java@gmail.com>
2022-11-04 08:44:22 +0300

    Task 1 copleted.

--- src/Tasks/Task1.java
+++ src/Tasks/Task1.java
@@ -5,37 +5,32 @@ import java.util.Scanner;
 public class Task1 {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int num = 0;
 
+        int score = 0;
+        String banderOstap = "Вы прямо герой труда";
+
+        Scanner scan = new Scanner(System.in);
         System.out.println("Введите любое положительное целое число ");
         int number = scan.nextInt();
         System.out.println("Вы ввели число " + number);
-        String banderOstap = "Вы прямо герой труда";
 
         if (number > 0 && number < 15) {
-            System.out.println("Группа " + ++num);
+            System.out.println("Группа 1");
+            score = 1;
         } else if (number >= 15 && number < 30) {
-            num+=2;
-            System.out.println("Группа " + num);
+            score = 2;
+            System.out.println("Группа 2");
         } else if (number >= 30) {
-
-            num+=3;
-            System.out.println("Группа " + num);
+            score = 3;
+            System.out.println("Группа 3");
         }
         else {
-            System.out.println("Вы ввели невереное число ");
+            System.out.println("Вы ввели неверное число ");
         }
-        if (num == 1) {
-            System.out.println(banderOstap);
-        } else if (num == 2) {
-            for (int i = 0; i < num; i++) {
+
+            for (int i = 0; i < score; i++) {
                 System.out.println(banderOstap);
             }
-                } else if (num ==3) {
-            for (int i = 0; i < num; i++) {
-                System.out.println(banderOstap);
-            }
-        }
     }
 }
+
